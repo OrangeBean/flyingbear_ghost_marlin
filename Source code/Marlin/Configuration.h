9fd1dfa52b414e6aed56006eba70759aa4c1f003
Orange Bean <contact@orangebean.online>
2024-08-07 09:58:57 +1000

    fix attempt 9

--- Source code/Marlin/Configuration.h
+++ Source code/Marlin/Configuration.h
@@ -2292,9 +2292,9 @@
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 99.8133
-  #define XY_DIAG_BD 100.0733
-  #define XY_SIDE_AD 70.6901
+  #define XY_DIAG_AC 100.0767
+  #define XY_DIAG_BD 99.9133
+  #define XY_SIDE_AD 70.6936
 
   // Or, set the XY skew factor directly:
   //#define XY_SKEW_FACTOR 0.0
--- Source code/Marlin/FlyingbearGhost_ConfigFile.h
+++ Source code/Marlin/FlyingbearGhost_ConfigFile.h
@@ -48,7 +48,7 @@ NANO13S_STOCK   -    MKS Robin Nano 1.3s - Driver 4x TMC2225
 
  #define FILAMENT_RUNOUT_SENSOR                               //<------- Enable or disable filament runout sensor
 
- #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.28, 80.48, 398.48, 397.60 }   //<------- Define your step/mm
+ #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.30, 80.50, 398.38, 397.48 }   //<------- Define your step/mm
 
  #define PIDTEMP                                               //<------- Define your PIDTEMP
   #define DEFAULT_Kp  22.20
